69726660e3ffd01c2cf29808a4a98b71b10788c2
たびチュの狐狸先生 <47691322+senkoraku552@users.noreply.github.com>
2024-05-18 13:45:25 +0800

    Update HeroIcons.jsx

--- src/components/HeroIcons.jsx
+++ src/components/HeroIcons.jsx
@@ -9,7 +9,7 @@ const HeroIcons = ({ isMobile, isTablet }) => {
 
   // def base class for icon positioning
   const baseIconPosition =
-    "absolute xs:bottom-20 sm:bottom-16 md:bottom-6 bottom-16 w-full flex";
+    "absolute xs:bottom-22 sm:bottom-18 md:bottom-6 bottom-16 w-full flex";
 
   // re-size for the scroll icon
   const scrollIconSize =
