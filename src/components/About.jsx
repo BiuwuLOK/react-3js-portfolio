87e23dab13d092b1e96b379420293f0ab68d8add
たびチュの狐狸先生 <47691322+senkoraku552@users.noreply.github.com>
2024-05-22 02:48:42 +0800

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,8 +6,34 @@ import { styles } from "../styles";
 import { services } from "../constants";
 import { fadeIn, textVariant } from "../utils/motion";
 
+const ServiceCard = () => {
+  return <></>;
+};
+
 const About = () => {
-  return <div>About</div>;
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>INTRODUCTION</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+
+      <motion.p
+        variants={fadeIn("", "", 0.2, 1)}
+        className="mt-4 text-secondary text-[16px] max-w-3xl leading-[30px]"
+      >
+        blablablablablablabla. blablablablablablabla blablablablablablabla
+        blablablablablablabla blablablablablablabla. blablablablablablabla
+        blablablablablablabla blablablablablablabla blablablablablablabla.
+        blablablablablablabla blablablablablablabla blablablablablablabla
+      </motion.p>
+      <div className="mt-20 flex flex-wrap gap-10">
+        {services.map((service, index) => {
+          <ServiceCard key={service.title} index={index} {...service} />;
+        })}
+      </div>
+    </>
+  );
 };
 
 export default About;
