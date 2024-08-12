1aec93492b3bcd7a90e9e5d4d8064b40757821d6
たびチュの狐狸先生 <47691322+senkoraku552@users.noreply.github.com>
2024-08-12 11:25:51 +0800

    Update Stars.jsx

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -5,9 +5,20 @@ import * as random from "maath/random/dist/maath-random.esm";
 
 const Stars = (props) => {
   const ref = useRef();
+
+  const [positions] = random.inSphere(new Float32Array(5001), { radius: 1.2 });
+  for (let i = 0; i < positions.length; i++) {
+    if (isNaN(positions[i])) {
+      console.error(`NaN value found at index ${i}`);
+      positions[i] = 0; // Replace NaN with a default value
+    }
+  }
+
+  /* 
   const [sphere] = useState(() =>
     random.inSphere(new Float32Array(5001), { radius: 1.2 })
   );
+ */
 
   useFrame((state, delta) => {
     ref.current.rotation.x -= delta / 10;
@@ -16,7 +27,13 @@ const Stars = (props) => {
 
   return (
     <group rotation={[0, 0, Math.PI / 4]}>
-      <Points ref={ref} positions={sphere} stride={3} frustumCulled {...props}>
+      <Points
+        ref={ref}
+        positions={positions}
+        stride={3}
+        frustumCulled
+        {...props}
+      >
         <PointMaterial
           transparent
           color="#f272c8"
@@ -32,7 +49,10 @@ const Stars = (props) => {
 const StarsCanvas = () => {
   return (
     <div className="w-full h-auto absolute inset-0 z-[-1]">
-      <Canvas camera={{ position: [0, 0, 1] }}>
+      <Canvas
+        gl={{ preserveDrawingBuffer: true }}
+        camera={{ position: [0, 0, 1] }}
+      >
         <Suspense fallback={null}>
           <Stars />
         </Suspense>
